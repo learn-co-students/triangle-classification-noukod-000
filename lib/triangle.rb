c564cb845c505bece4e379cd48c45d830eab7df8
Saint-Jean Germila <germila@noukod.com>
2018-10-23 15:23:26 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  attr_accessor :equilateral, :isosceles ,:scalene
+
+  def initailize(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+
+  def kind(triangle)
+    self.equilateral = triangle
+    self.isosceles = triangle
+    self.scalene = triangle
+    if triangle.class != Triangle
+      begin
+        raise TriangleError
+        rescue  TriangleError  => error
+          puts error.message
+        end
+      else
+        triangle.equilateral = self
+        triangle.isosceles = self
+        triangle.scalene = self
+   end
+ end
+end
+
+class TriangleError < StandardError
+ def message
+   "ERROR"
+  end
+ end
 end
