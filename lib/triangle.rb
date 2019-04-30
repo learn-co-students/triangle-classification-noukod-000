a76c17a5af8c6b30798112a42fecd179c9dcbc41
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-30 14:32:30 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,34 +1,34 @@
 class Triangle
-  attr_accessor :s_1, :s_2, :s_3
+  attr_accessor :side_1, :side_2, :side_3
+  def initialize(side_1 ,side_2 ,side_3 )
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
 
-    def initialize(s_1, s_2, s_3)
-      @s_1 = s_1
-      @s_2 = s_2
-      @s_3 = s_3
-    end
-
-  def kind
-    restriction = (@s_1 > 0),
-                  (@s_2 > 0),
-                  (@s_3 > 0),
-                  (@s_1  + @s_2 > @s_3),
-                  (@s_1  + @s_3 > @s_2),
-                  (@s_3 + @s_2 > @s_1)
-
-    if restriction.include?(false)
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
-    end
-
-    if @s_1 == @s_2 && @s_2 == @s_3
-      :equilateral
-    elsif @s_1 == @s_2 || @s_1 == @s_3 || @s_2 == @s_3
-      :isosceles
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
     else
-      :scalene
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
+
+  end
+  
+  class TriangleError < StandardError
+   #triangle error code
+    def message1
+      
     end
   end
 
-  class TriangleError < StandardError
-  end
 end
 
