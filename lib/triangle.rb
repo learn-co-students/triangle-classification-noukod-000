903176e1d44361871d72f6cbf3e2bb53cb728753
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-30 14:25:53 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,11 @@ class Triangle
 
   def kind()
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      #begin
-        #raise TriangleError
-      #rescue TriangleError => error
-          #puts error.message1
-      #end
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message1
+      end
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
@@ -29,11 +29,11 @@ class Triangle
   end
   
   class TriangleError < StandardError
-  # triangle error code
-  def message1
-    
+   #triangle error code
+    def message1
+      
+    end
   end
-end
 
 end
 
